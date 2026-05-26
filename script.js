3c716f6b83b9d9198a5dc325e0fc3c35dd52af70
Rudra pratap singh <rudra@gmail.com>
2026-05-27 01:49:13 +0530

    bug fixed

--- script.js
+++ script.js
@@ -33,6 +33,13 @@ add_btn.addEventListener("click",function(){
     div.appendChild(span);
 
     const delete_btn = document.createElement("button");
+    delete_btn.addEventListener("click",function(){
+        // console.log(this);
+        // getting the li
+
+        let parent = this.parentElement;
+        parent.remove();
+    })
     delete_btn.classList.add("delete-btn");
     delete_btn.textContent = "Delete";
     li.appendChild(delete_btn);
@@ -43,21 +50,7 @@ add_btn.addEventListener("click",function(){
     input.value = "";
 });
 
-// delete feature:
 
-// here i need to delete a list in ul when user click that particular delete button
-
-const delete_btn = document.getElementsByClassName("delete-btn");
-// delete_btn -> html collection
-
-
-for(let i=0;i<delete_btn.length;i++){
-    delete_btn[i].addEventListener("click",function(){
-        // console.log(this);
-        // getting the li
-
-        let parent = this.parentElement;
-        parent.remove();
-    })
-}
+// when we createElement then it get added to ram (js memory) , browser -> nothing
+// when we do append child then it get added to html --> dom
 
